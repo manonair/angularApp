ebc1ba58e34ea623518833d23dab671b2c75d27e
Manoj Nair <manonair20@gmail.com>
2019-10-20 18:04:25 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-docker run -p 8000:80 manonair/angularapp
+docker stop angularapp && \
+docker rm angularapp && \
+docker pull manonair/angularapp && \
+docker run -d --name=angularapp -p 8000:80 manonair/angularapp
